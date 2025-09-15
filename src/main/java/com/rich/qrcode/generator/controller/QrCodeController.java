687adb8e33969225e0d6a580ad3e69940ba76489
richard matheus <richard.tech99@gmail.com>
2025-09-14 22:20:38 -0300

    first commit

---
+++ src/main/java/com/rich/qrcode/generator/controller/QrCodeController.java
@@ -0,0 +1,30 @@
+package com.rich.qrcode.generator.controller;
+
+import com.rich.qrcode.generator.dto.QrCodeGenerateRequest;
+import com.rich.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.rich.qrcode.generator.service.QrCodeGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/qrcode")
+public class QrCodeController {
+
+    @Autowired
+    private QrCodeGeneratorService qrCodeGeneratorService;
+
+    @PostMapping
+    public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
+            try {
+                QrCodeGenerateResponse response =  this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+                return ResponseEntity.ok(response);
+            } catch (Exception e) {
+                 System.out.println(e);
+                 return ResponseEntity.internalServerError().build();
+            }
+    }
+}
